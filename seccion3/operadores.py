3b934269897467837daafc391b27553d34dcd9fa
tati0906 <tatianavanegas2006@gmail.com>
2026-04-21 16:19:43 -0500

    Seccion 3: verificacion de ejercicios de operadores

---
+++ seccion3/operadores.py
@@ -0,0 +1,17 @@
+# Sección 3 - Verificación de ejercicios
+
+print("Ejercicio 1:", 5 + 3 * 2)
+print("Ejercicio 2:", 8 / 2 + 4 * 3)
+print("Ejercicio 3:", (7 + 3) * 2 - 5)
+print("Ejercicio 4:", 10 - 4 + 2 * 3)
+print("Ejercicio 5:", (10 / 2) * (3 + 2) - 4)
+print("Ejercicio 6:", 2 + 3 * (4 - 1))
+print("Ejercicio 7:", 5 * 2 ** 3)
+print("Ejercicio 8:", 6 + 4 / 2 ** 2)
+print("Ejercicio 9:", 10 % 3 + 2 * 5)
+print("Ejercicio 10:", (8 + 2) * 3 ** 2)
+print("Ejercicio 11:", 7 + 2 * (3 + 5) / 4)
+print("Ejercicio 12:", 2 ** 3 * 4 / 2)
+print("Ejercicio 13:", 9 - 6 + 3 ** 2)
+print("Ejercicio 14:", (7 - 2) * 5 + 3 ** 2)
+print("Ejercicio 15:", 4 * 2 ** 3 / 8 + 1)
