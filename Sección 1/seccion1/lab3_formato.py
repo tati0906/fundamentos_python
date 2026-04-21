88f945a41c33a51d5c893e5b4a6b1a6eab3df97e
tati0906 <tatianavanegas2006@gmail.com>
2026-04-21 16:06:25 -0500

    LAB3: implementacion de formato de salida con saltos de linea y duplicacion

---
+++ Sección 1/seccion1/lab3_formato.py
@@ -0,0 +1,21 @@
+# LAB 3 - Dando formato a la salida
+
+# Flecha usando un solo print con saltos de línea (\n)
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+
+print()
+
+# Flecha duplicada (vertical)
+print(("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****\n") * 2)
+
+print()
+
+# Flecha doble horizontal
+print("    *    " * 2)
+print("   * *   " * 2)
+print("  *   *  " * 2)
+print(" *     * " * 2)
+print("***   ***" * 2)
+print("  *   *  " * 2)
+print("  *   *  " * 2)
+print("  *****  " * 2)
